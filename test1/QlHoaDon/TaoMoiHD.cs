28b6f8d528e8f9bfc045fe5fecb83e371b642c93
yennguyen <136480710+tocnhan@users.noreply.github.com>
2024-11-30 08:38:21 +0700

    nhap so luong hang hoa can ban

--- test1/QlHoaDon/TaoMoiHD.cs
+++ test1/QlHoaDon/TaoMoiHD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,15 +14,58 @@ namespace test1.QlHoaDon
     public partial class TaoMoiHD : Form
     {
         private int id;
+        string MysqlCon = " server=127.0.0.1; user=root; database=quanlysieuthi; password= ";
         public TaoMoiHD(int id)
         {
             InitializeComponent();
             this.id = id;
+            dt_sp.CellClick += dataGridView2_CellClick;
+            dt_addsp.CellClick += dataGridView1_CellClick;
+
+            // Thêm cột nút vào DataGridView
+            AddButtonColumnToDataGridView();
+            AddButtonColumnToDataGridView1();
         }
-
-        private void TaoMoiHD_Load(object sender, EventArgs e)
+        private void LoadDataGrid()
         {
+            using (MySqlConnection conn = new MySqlConnection(MysqlCon))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT id, tenhang, dongia FROM hanghoa"; // Truy vấn lấy dữ liệu từ bảng
+                    string query_selected = $"SELECT hd_sp.id AS id,  hanghoa.tenhang AS tenhang, hd_sp.soluong AS soluong FROM hd_sp JOIN hanghoa ON hd_sp.id_sp = hanghoa.id WHERE hd_sp.id_hd = {id};";
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
 
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Gắn dữ liệu vào DataGridView
+                    dt_addsp.DataSource = dt;
+
+                    // Tùy chỉnh tiêu đề cột (nếu cần)
+                    dt_addsp.Columns["id"].HeaderText = "ID";
+                    dt_addsp.Columns["tenhang"].HeaderText = "Tên hàng";
+                    dt_addsp.Columns["dongia"].HeaderText = "đơn giá";
+
+
+                    MySqlDataAdapter da_sl = new MySqlDataAdapter(query_selected, conn);
+                    DataTable dt_sl = new DataTable();
+                    da_sl.Fill(dt_sl);
+
+                    // Gắn dữ liệu vào DataGridView
+                    dt_sp.DataSource = dt_sl;
+
+                    // Tùy chỉnh tiêu đề cột (nếu cần)
+                    dt_sp.Columns["id"].HeaderText = "ID";
+                    dt_sp.Columns["tenhang"].HeaderText = "Tên hàng";
+                    dt_sp.Columns["soluong"].HeaderText = "số lượng";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                }
+            }
         }
         private void AddButtonColumnToDataGridView()
         {
@@ -35,5 +79,90 @@ namespace test1.QlHoaDon
             // Thêm cột vào DataGridView
             dt_sp.Columns.Add(buttonColumn);
         }
+        private void AddButtonColumnToDataGridView1()
+        {
+            // Tạo cột kiểu nút
+            DataGridViewButtonColumn buttonColumn1 = new DataGridViewButtonColumn();
+            buttonColumn1.Name = "Action";
+            buttonColumn1.HeaderText = "Thao tác";
+            buttonColumn1.Text = "bỏ"; // Văn bản hiển thị trên nút
+            buttonColumn1.UseColumnTextForButtonValue = true; // Dùng văn bản mặc định cho nút
+
+            // Thêm cột vào DataGridView
+            dt_addsp.Columns.Add(buttonColumn1);
+        }
+        private void dataGridView2_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == dataGridView2.Columns["Action"].Index)
+            {
+                // Lấy giá trị ID từ cột "ID" của dòng tương ứng
+                int id_sp = Convert.ToInt32(dataGridView2.Rows[e.RowIndex].Cells["ID"].Value);
+                string name = Convert.ToString(dataGridView2.Rows[e.RowIndex].Cells["tenhang"].Value);
+                // Hiển thị hoặc xử lý giá trị ID
+                using (input_Soluong = new input_soluong())
+                {
+                    if (input_Soluong.ShowDialog() == DialogResult.OK)
+                    {
+                        // Lấy giá trị số được nhập
+                        int enteredNumber = input_Soluong.EnteredNumber;
+                        MessageBox.Show($"Bạn đã nhập số: {enteredNumber}");
+                        using (MySqlConnection conn = new MySqlConnection(MysqlCon))
+                        {
+                            try
+                            {
+                                conn.Open();
+
+                                // Câu lệnh INSERT
+                                string query = "INSERT INTO hd_sp (id_sp, id_hd, soluong) VALUES (@id, @id_hd, @soluong);";
+                                MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                                // Gắn giá trị vào các tham số
+                                cmd.Parameters.AddWithValue("@id", id_sp);
+                                cmd.Parameters.AddWithValue("@id_hd", id);
+                                cmd.Parameters.AddWithValue("@soluong", enteredNumber);
+
+                                // Thực thi lệnh
+                                int rowsInserted = cmd.ExecuteNonQuery();
+
+                                if (rowsInserted > 0)
+                                {
+                                    MessageBox.Show("Thêm mới thành công!");
+                                    // Tải lại dữ liệu lên DataGridView (nếu có)
+
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Không có thay đổi nào được thực hiện.");
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Lỗi: " + ex.Message);
+                            }
+                        }
+                        // Hiển thị số đã nhập
+
+                    }
+                }
+
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == dataGridView1.Columns["Action"].Index)
+            {
+                // Lấy giá trị ID từ cột "ID" của dòng tương ứng
+                int id_selec = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells["ID"].Value);
+                delete_selected(id_selec);
+
+
+            }
+        }
+        private void TaoMoiHD_Load(object sender, EventArgs e)
+        {
+
+        }
+        
     }
 }
---
+++ test1/QlHoaDon/soluong.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace test1.QlHoaDon
+{
+    partial class soluong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "nhập số lượng";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(155, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 30);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(18, 85);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "xác nhận";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(353, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 33);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "hủy";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // soluong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 130);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "soluong";
+            this.Text = "soluong";
+            this.Load += new System.EventHandler(this.soluong_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
