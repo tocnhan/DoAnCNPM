d3db558d3af134e79ffc224e887701622f7d7249
yennguyen <136480710+tocnhan@users.noreply.github.com>
2024-12-01 00:13:06 +0700

    done

--- test1/thongke.Designer.cs
+++ test1/thongke.Designer.cs
@@ -28,35 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.splitter1);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 53);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(386, 192);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.Size = new System.Drawing.Size(288, 39);
             this.label1.TabIndex = 0;
-            this.label1.Text = "thong ke";
+            this.label1.Text = "thống kê tháng 11";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(3, 3);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(310, 33);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 59);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 309);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(635, 374);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "in thống kê";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // thongke
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "thongke";
             this.Text = "thongke";
             this.Load += new System.EventHandler(this.thongke_Load);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Splitter splitter1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- test1/thongke.cs
+++ test1/thongke.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,65 @@ namespace test1
 {
     public partial class thongke : Form
     {
+        string MysqlCon = " server=127.0.0.1; user=root; database=quanlysieuthi; password= ";
         public thongke()
         {
             InitializeComponent();
+            LoadDataGrid();
         }
 
         private void thongke_Load(object sender, EventArgs e)
         {
             this.ControlBox = false;
+        }
+        private void LoadDataGrid()
+        {
+            using (MySqlConnection conn = new MySqlConnection(MysqlCon))
+            {
+                try
+                {
+                    conn.Open();
+
+                    // Truy vấn JOIN để lấy dữ liệu từ 2 bảng
+                    string query = @"
+                SELECT 
+                    hanghoa.tenhang AS 'Tên Hàng', 
+                    hd_sp.soluong AS 'Số Lượng', 
+                    hanghoa.dongia AS 'Đơn Giá', 
+                    (hd_sp.soluong * hanghoa.dongia) AS 'Thành Tiền'
+                FROM 
+                    hd_sp
+                JOIN 
+                    hanghoa ON hd_sp.id_sp = hanghoa.id";
+
+                    // Sử dụng MySqlDataAdapter để lấy dữ liệu
+                    MySqlDataAdapter da = new MySqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    // Gắn dữ liệu vào DataGridView
+                    dataGridView1.DataSource = dt;
+
+                    // Tùy chỉnh tiêu đề cột (nếu cần)
+                    dataGridView1.Columns["Tên Hàng"].HeaderText = "Tên Hàng";
+                    dataGridView1.Columns["Số Lượng"].HeaderText = "Số Lượng";
+                    dataGridView1.Columns["Đơn Giá"].HeaderText = "Đơn Giá";
+                    dataGridView1.Columns["Thành Tiền"].HeaderText = "Thành Tiền";
+
+                    // Định dạng tiền tệ cho cột "Thành Tiền"
+                    dataGridView1.Columns["Thành Tiền"].DefaultCellStyle.Format = "C";
+                }
+                catch (Exception ex)
+                {
+                    // Hiển thị thông báo lỗi nếu xảy ra lỗi
+                    MessageBox.Show("Lỗi: " + ex.Message);
+                }
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
